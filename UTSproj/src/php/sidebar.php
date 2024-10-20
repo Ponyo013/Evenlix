888848e93169ae35c26272ab76892b4277e3a489
Nevanoz <163624599+Nevanoz@users.noreply.github.com>
2024-10-20 15:08:39 +0700

    w

--- UTSproj/src/php/sidebar.php
+++ UTSproj/src/php/sidebar.php
@@ -3,7 +3,7 @@
   <!-- Sidebar scroll -->
   <div>
     <div class="d-flex align-items-center">
-      <a href="./index.php" class="">
+      <a href="./index.php">
         <img class="logo" src="../assets/images/logos/logo.png" alt="Logo" style="width: 250px; height: auto;" />
       </a>
       <div class="close-btn d-xl-none d-block sidebartoggler cursor-pointer" id="sidebarCollapse">
@@ -11,17 +11,18 @@
       </div>
     </div>
 
-
-    <!-- Sidebar navigation-->
-    <nav class="sidebar-nav scroll-sidebar" data-simplebar="">
+    <!-- Sidebar navigation -->
+    <nav class="sidebar-nav scroll-sidebar" data-simplebar>
       <ul id="sidebarnav">
+
+        <!-- Home Section -->
         <li class="nav-small-cap">
           <span class="hide-menu">Home</span>
         </li>
         <li class="sidebar-item">
           <a class="sidebar-link" href="./index.php" aria-expanded="false">
             <span>
-              <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="icon icon-tabler icons-tabler-outline icon-tabler-home">
+              <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="icon icon-tabler icon-tabler-home">
                 <path stroke="none" d="M0 0h24v24H0z" fill="none" />
                 <path d="M5 12l-2 0l9 -9l9 9l-2 0" />
                 <path d="M5 12v7a2 2 0 0 0 2 2h10a2 2 0 0 0 2 -2v-7" />
@@ -30,16 +31,18 @@
             </span>
             <span class="hide-menu">Dashboard</span>
           </a>
-
         </li>
 
+        <!-- Admin Section -->
         <li class="nav-small-cap">
           <span class="hide-menu">Admin</span>
         </li>
+
+        <!-- Add Event -->
         <li class="sidebar-item">
           <a class="sidebar-link" href="./addevent.php" aria-expanded="false">
             <span>
-              <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="currentColor" class="icon icon-tabler icons-tabler-filled icon-tabler-circle-plus">
+              <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="currentColor" class="icon icon-tabler icon-tabler-circle-plus">
                 <path stroke="none" d="M0 0h24v24H0z" fill="none" />
                 <path d="M4.929 4.929a10 10 0 1 1 14.141 14.141a10 10 0 0 1 -14.14 -14.14zm8.071 4.071a1 1 0 1 0 -2 0v2h-2a1 1 0 1 0 0 2h2v2a1 1 0 1 0 2 0v-2h2a1 1 0 1 0 0 -2h-2v-2z" />
               </svg>
@@ -48,10 +51,11 @@
           </a>
         </li>
 
+        <!-- Edit Event -->
         <li class="sidebar-item">
           <a class="sidebar-link" href="./editevent.php" aria-expanded="false">
             <span>
-              <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="icon icon-tabler icons-tabler-outline icon-tabler-edit">
+              <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="icon icon-tabler icon-tabler-edit">
                 <path stroke="none" d="M0 0h24v24H0z" fill="none" />
                 <path d="M7 7h-1a2 2 0 0 0 -2 2v9a2 2 0 0 0 2 2h9a2 2 0 0 0 2 -2v-1" />
                 <path d="M20.385 6.585a2.1 2.1 0 0 0 -2.97 -2.97l-8.415 8.385v3h3l8.385 -8.415z" />
@@ -62,22 +66,24 @@
           </a>
         </li>
 
+        <!-- Delete Event -->
         <li class="sidebar-item">
           <a class="sidebar-link" href="./deleteevent.php" aria-expanded="false">
             <span>
-              <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="icon icon-tabler icons-tabler-outline icon-tabler-circle-minus">
+              <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="icon icon-tabler icon-tabler-circle-minus">
                 <path stroke="none" d="M0 0h24v24H0z" fill="none" />
                 <path d="M12 12m-9 0a9 9 0 1 0 18 0a9 9 0 1 0 -18 0" />
                 <path d="M9 12l6 0" />
               </svg>
             </span>
-            <span class="hide-menu">Delete</span>
+            <span class="hide-menu">Delete Event</span>
           </a>
         </li>
+
       </ul>
     </nav>
     <!-- End Sidebar navigation -->
   </div>
   <!-- End Sidebar scroll-->
 </aside>
-<!--  Sidebar End -->
+<!-- Sidebar End -->
