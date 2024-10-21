c80c6f95a50d7b6ac9198a028b4dd9c6df889e55
Ponyo013 <163632247+Ponyo013@users.noreply.github.com>
2024-10-21 11:29:37 +0700

    db push

--- UTSproj/src/php/sidebar.php
+++ UTSproj/src/php/sidebar.php
@@ -116,11 +116,7 @@
           <li class="sidebar-item">
             <a class="sidebar-link" href="./viewevents.php" aria-expanded="false">
               <span>
-                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="icon icon-tabler icons-tabler-outline icon-tabler-circle-minus">
-                  <path stroke="none" d="M0 0h24v24H0z" fill="none" />
-                  <path d="M12 12m-9 0a9 9 0 1 0 18 0a9 9 0 1 0 -18 0" />
-                  <path d="M9 12l6 0" />
-                </svg>
+                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path fill="currentColor" d="M16 21q-.825 0-1.412-.587T14 19v-4q0-.825.588-1.412T16 13h4q.825 0 1.413.588T22 15v4q0 .825-.587 1.413T20 21zm0-2h4v-4h-4zM2 18v-2h9v2zm14-7q-.825 0-1.412-.587T14 9V5q0-.825.588-1.412T16 3h4q.825 0 1.413.588T22 5v4q0 .825-.587 1.413T20 11zm0-2h4V5h-4zM2 8V6h9v2zm16-1"/></svg>
               </span>
               <span class="hide-menu">Available Events</span>
             </a>
